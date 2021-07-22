de176ad70f356fdc092b8e5044bf400d0c62c02c
Victor Salcedo <torvic02st96@gmail.com>
2021-07-22 14:52:09 -0500

    Feat(project): Add custom transition for sections in recommendation page

--- sections/Private/Recommentation/CategoryActivitySection/index.jsx
+++ sections/Private/Recommentation/CategoryActivitySection/index.jsx
@@ -4,7 +4,7 @@ import Button from 'components/Button'
 import H2 from 'components/H2'
 import styles from './index.module.scss'
 
-const CategoryActivities = ({ dbCategory, dbActivity }) => {
+const CategoryActivities = ({ dbCategory, dbActivity, handleClickNext, handleClickBefore }) => {
   const AddCategoryAndActivitySelected = (data) => {
     // update
     console.log(data)
@@ -37,8 +37,8 @@ const CategoryActivities = ({ dbCategory, dbActivity }) => {
           ))}
       </div>
       <div>
-        <Button onClick={() => console.log('Atrás')}>Atrás</Button>
-        <Button onClick={() => console.log('Siguiente')}>Siguiente</Button>
+        <Button onClick={handleClickBefore}>Atrás</Button>
+        <Button onClick={handleClickNext}>Siguiente</Button>
       </div>
     </section>
   )
--- sections/Private/Recommentation/OtherActivitiesSection/index.jsx
+++ sections/Private/Recommentation/OtherActivitiesSection/index.jsx
@@ -1,16 +1,37 @@
 import React from 'react'
 import H2 from 'components/H2'
 import CustomTooltip from 'components/CustomTooltip'
+import Button from 'components/Button'
+import styles from './index.module.scss'
 import { Select } from 'antd'
 
-const OtherActivitiesSection = () => {
+const { Option } = Select
+
+const OtherActivitiesSection = ({ handleClickBefore }) => {
   return (
-    <section>
+    <section className={styles.section} >
       <H2>Otras Actividades</H2>
       <CustomTooltip
         text="¿Algun lugar especifico que quieras ver, restaurantes que quieras visitar o cosas que quieras hacer?"
         title="Extra information"
       />
+      <Select
+        mode="multiple"
+        placeholder="Destinos"
+        style={{ width: '45%', marginTop: '20px' }}
+      >
+        <Option value='Lineas de Nazca' label='Lineas de Nazca' >Lineas de Nazca</Option>
+        <Option value='Canotaje' label='Canotaje' >Canotaje</Option>
+        <Option value='Machu picchu' label='Machu picchu' >Machu picchu</Option>
+        <Option value='Parapente' label='Parapente' >Parapente</Option>
+        <Option value='Lunahuaná' label='Lunahuaná' >Lunahuaná</Option>
+        <Option value='Qenqo' label='Qenqo' >Qenqo</Option>
+      </Select>
+      <div style={{ marginTop: '100px' }} >
+        <Button onClick={handleClickBefore}>Atrás</Button>
+        <Button onClick={() => console.log('Siguiente')}>Siguiente</Button>
+      </div>
+
     </section>
   )
 }
