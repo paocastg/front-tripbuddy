f4349d42002a075e2e69b2cc48fc95d80749b45f
Victor Salcedo <torvic02st96@gmail.com>
2021-07-22 14:52:09 -0500

    Feat(project): Add custom transition for sections in recommendation page

--- pages/Modules/Private/RecommendationPage/index.jsx
+++ pages/Modules/Private/RecommendationPage/index.jsx
@@ -1,10 +1,13 @@
 import React, { useState, useEffect } from 'react'
 import styles from './index.module.scss'
+import Button from 'components/Button'
 import CategoryActivities from 'sections/Private/Recommentation/CategoryActivitySection'
 import OtherActivitiesSection from 'sections/Private/Recommentation/OtherActivitiesSection'
 const axios = require('axios')
 
 const RecommendationPage = () => {
+  const [toggleOne, setToggleOne] = useState(false)
+  const [toggleTwo, setToggleTwo] = useState(false)
   const [dbCategory, setDbCategory] = useState(null)
   const [dbActivity, setDbActivity] = useState(null)
 
@@ -38,10 +41,47 @@ const RecommendationPage = () => {
     getCategory()
     getActivity()
   }, [])
+
+  const handleToggleOne = () => {
+    setToggleOne((prev) => !prev)
+  }
+
+  const handleToggleTwo = () => {
+    setToggleTwo((prev) => !prev)
+  }
+
   return (
-    <div className={styles.main} >
-      <CategoryActivities dbCategory={dbCategory} dbActivity={dbActivity} />
-      <OtherActivitiesSection/>
+    <div className={styles.main}>
+      <div
+        className={
+          toggleOne ? styles.isActiveForm : styles.hiddenSectionCategory
+        }
+      >
+        <h2>Formulario de Destinos</h2>
+        <section>
+          <Button onClick={() => console.log('Atrás')}>Atrás</Button>
+          <Button onClick={handleToggleOne}>Siguiente</Button>
+        </section>
+      </div>
+      <div
+        className={
+          toggleTwo
+            ? styles.hiddenSectionCategory
+            : toggleOne
+              ? styles.hiddenSectionForm
+              : styles.isActiveCategory
+        }
+      >
+        <CategoryActivities
+          dbCategory={dbCategory}
+          dbActivity={dbActivity}
+          handleClickNext={handleToggleTwo}
+          handleClickBefore={handleToggleOne}
+        />
+      </div>
+      <div className={toggleTwo ? styles.isActive : styles.hiddenSection}>
+        <OtherActivitiesSection handleClickBefore={handleToggleTwo} />
+      </div>
     </div>
   )
 }
