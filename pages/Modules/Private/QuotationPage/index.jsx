44ed460a325d01684f5432f3e53b788788778ba5
torvic <130111@unsaac.edu.pe>
2021-09-04 05:18:24 -0500

    Feat(QuotationPage): I create a table of open quotations

--- pages/Modules/Private/QuotationPage/index.jsx
+++ pages/Modules/Private/QuotationPage/index.jsx
@@ -1,13 +1,48 @@
+import { Table, Tag, Space } from 'antd'
 import H2 from 'components/H2'
 import Wrapper from 'layout/Wrapper'
 import styles from './index.module.scss'
 
 const QuotationPage = () => {
+  const columns = [
+    {
+      title: 'Agencia',
+      dataIndex: 'agencia',
+      key: 'agencia',
+      render: (text) => <a>{text}</a>
+    },
+    {
+      title: 'Descripcion',
+      dataIndex: 'descripcion',
+      key: 'descripcion'
+    },
+    {
+      title: 'Destinos',
+      dataIndex: 'destinos',
+      key: 'destinos'
+    },
+    {
+      title: 'Precio',
+      dataIndex: 'precio',
+      key: 'precio'
+    }
+
+  ]
+
+  const data = [
+    {
+      key: '1',
+      agencia: 'Agencia Turismo',
+      descripcion: 'Lorem Ipsum ....',
+      destinos: 'Centro de Lima, Choquequirao, ...',
+      precio: '$ 2000 c/u'
+    }
+  ]
   return (
     <Wrapper>
       <div className={styles.margin}>
         <H2>Cotizaciones Abiertas</H2>
-        <p>proximamente ...</p>
+        <Table columns={columns} dataSource={data} />
       </div>
     </Wrapper>
   )
