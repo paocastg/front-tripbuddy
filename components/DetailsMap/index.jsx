c95fcd47824b8682bee3075c6f9c93da41a6943b
torvic <130111@unsaac.edu.pe>
2021-09-27 22:34:50 -0500

    Feat: Agregue el componente DetailsMap.

---
+++ components/DetailsMap/index.jsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import {
+  GoogleMap,
+  withScriptjs,
+  withGoogleMap,
+  Marker
+} from 'react-google-maps'
+
+const DetailsMap = (props) => {
+  return (
+    <div>
+      <GoogleMap
+        defaultZoom={5}
+        defaultCenter={{ lat: -9.189967, lng: -75.015152 }}
+        defaultOptions={{ disableDefaultUI: true }}
+      >
+        {props.destinosCompleto.map((marker) => {
+          return (
+            <Marker
+              key={marker.id}
+              position={{ lat: parseFloat(marker.latitud), lng: parseFloat(marker.longitud) }}
+              title={marker.nombre}
+            />
+          )
+        })}
+      </GoogleMap>
+    </div>
+  )
+}
+
+export default withScriptjs(withGoogleMap(DetailsMap))
