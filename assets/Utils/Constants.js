004e8190f1a587fd994b7f1317984e56fa1aa436
torvic <130111@unsaac.edu.pe>
2021-09-28 19:06:23 -0500

    Feat: Actualice las constantes del proyecto.

--- assets/Utils/Constants.js
+++ assets/Utils/Constants.js
@@ -2,7 +2,7 @@
 // export const CDN_PATH = process.env.CDN_PATH
 export const CDN_PATH = 'https://file-loenviamostest.s3.amazonaws.com/images'
 // export const API = process.env.API
-export const API = 'http://apitripbuddy.devopsacademy.pe/api'
+export const API = 'https://api.devopsacademy.pe/api'
 export const API_TEST = 'http://localhost:8000/api'
-export const HOST = 'http://apitripbuddy.devopsacademy.pe'
+export const HOST = 'https://api.devopsacademy.pe'
 export const MAPS_KEY = 'AIzaSyBN79o66J0uzzF3g3ViCUc4CowEBgeEzwc'
