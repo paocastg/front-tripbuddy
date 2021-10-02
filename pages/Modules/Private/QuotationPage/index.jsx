3aa836f1ae2e3e9c8a4274e8ed965ea565a2c83c
torvic <130111@unsaac.edu.pe>
2021-10-01 23:59:12 -0500

    Fix: Agregue a axios para que llame a la api de manera dinamica.

--- pages/Modules/Private/QuotationPage/index.jsx
+++ pages/Modules/Private/QuotationPage/index.jsx
@@ -10,44 +10,47 @@ import Wrapper from 'layout/Wrapper'
 import { HOST } from 'assets/Utils/Constants'
 import axios from 'axios'
 import React, { useState, useEffect } from 'react'
+import { Auth } from 'assets/Utils/Auth'
 
 const QuotationPage = () => {
-  const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(true);
-  const [vacio, setVacio] = useState(false);
+  const [data, setData] = useState([])
+  const [loading, setLoading] = useState(true)
+  const [vacio, setVacio] = useState(false)
 
   const fetchUsers = async () => {
-    const response = await axios.get(HOST + "/solicitud/list_cotizaciones/10");
-    setData(response.data.solicitud);
-    setLoading(false);
-  };
+    const user = Auth.getSession().usuario.id
+    const response = await axios.get(HOST + '/solicitud/list_cotizaciones/' + user)
+    setData(response.data.solicitud)
+    setLoading(false)
+  }
 
   useEffect(() => {
-    fetchUsers();
-  }, []);
+    fetchUsers()
+  }, [])
 
   return (
     <Wrapper>
       <Session>
         <div className={styles.quotation}>
           <H2>Cotizaciones Abiertas</H2>
-          <div style = {{overflow: 'scroll'}}>
-          <Table bordered style = {{width:'1400px'}} >
+          <div style = {{ overflow: 'scroll' }}>
+          <Table bordered style = {{ width: '1400px' }} >
                   <thead>
-                    <tr style={{ background: "#00B2E3", color: "#fff", fontFamily:"Geomanist-Regular",  height: '60px' }}>                      
+                    <tr style={{ background: '#00B2E3', color: '#fff', fontFamily: 'Geomanist-Regular', height: '60px' }}>
                     <th>Agencia</th>
                       <th>Descripcion</th>
                       <th>Destinos</th>
                       <th>Precio</th>
                     </tr>
                     <tr><th></th><th></th></tr>
-                    <tr style={{ background: "#2C2C2C", color: "#fff", fontFamily:"Geomanist-Regular",}}>                      
+                    <tr style={{ background: '#2C2C2C', color: '#fff', fontFamily: 'Geomanist-Regular' }}>
                       <th></th>
                       <th></th>
                       <th>Categoria</th>
                       <th>Actividades</th>
-                      </tr>                 
-                    {loading ? (
+                      </tr>
+                    {loading
+                      ? (
                       <tr className="text-center">
                         <td colSpan="8">
                           <div
@@ -58,33 +61,38 @@ const QuotationPage = () => {
                           </div>
                         </td>
                       </tr>
-                    ) : vacio ? (
+                        )
+                      : vacio
+                        ? (
                       <tr className="text-center">
                         <td colSpan="8">No hay ningun registro</td>
                       </tr>
-                    ) : (
-                      data && data.map((item, index) => {
-                        return (                 
-                          <tr style={{                            
-                            background: "#2C2C2C", fontFamily:"Geomanist-Regular",
-                            color: "#fff",
-                            cursor: "pointer",
+                          )
+                        : (
+                            data && data.map((item, index) => {
+                              return (
+                          <tr style={{
+                            background: '#2C2C2C',
+                            fontFamily: 'Geomanist-Regular',
+                            color: '#fff',
+                            cursor: 'pointer'
                           }}>
-                           <center><td>{item.destino.map((destinos) => destinos.nombre) + "  " }</td></center>
+                           <center><td>{item.destino.map((destinos) => destinos.nombre) + '  ' }</td></center>
                           <th>
-                          <th>{ "   / " + ((moment(item.fecha_fin).diff(moment(item.fecha_inicio),  'dias'))/(1000*60*60*24)) + " dias "  }</th>
-                          <th>{ " / " + (item.numero_adultos+item.numero_adolescentes+item.numero_ninos) + " Personas "  }</th>
-                          </th>                          
-                          <th>{item.categoria.map((categorias) => categorias.nombre) + "  "  }</th>
-                          <th>{item.actividad.map((actividads) => actividads.nombre) + " "}</th>             
+                          <th>{ '   / ' + ((moment(item.fecha_fin).diff(moment(item.fecha_inicio), 'dias')) / (1000 * 60 * 60 * 24)) + ' dias ' }</th>
+                          <th>{ ' / ' + (item.numero_adultos + item.numero_adolescentes + item.numero_ninos) + ' Personas ' }</th>
+                          </th>
+                          <th>{item.categoria.map((categorias) => categorias.nombre) + '  ' }</th>
+                          <th>{item.actividad.map((actividads) => actividads.nombre) + ' '}</th>
                           </tr>
-                      );
-                      })
-                    )}     
+                              )
+                            })
+                          )}
                     <tr><th></th></tr><br></br>
                   </thead>
                   <tbody>
-                    {loading ? (
+                    {loading
+                      ? (
                       <tr className="text-center">
                         <td colSpan="8">
                           <div
@@ -95,26 +103,29 @@ const QuotationPage = () => {
                           </div>
                         </td>
                       </tr>
-                    ) : vacio ? (
+                        )
+                      : vacio
+                        ? (
                       <tr className="text-center">
                         <td colSpan="8">No hay ningun registro</td>
                       </tr>
-                    ) : (
-                      data && data.map((item, index) => {
-                        return (
-                          <tr key={index} 
+                          )
+                        : (
+                            data && data.map((item, index) => {
+                              return (
+                          <tr key={index}
                           style={{
-                            fontFamily:"Geomanist-Regular",
-                            cursor: "pointer",
+                            fontFamily: 'Geomanist-Regular',
+                            cursor: 'pointer'
                           }}>
-                            <td><center>{item.cotizaciones.map((cotizacion) => cotizacion.agencia) + "  "}</center></td>
-                            <td><center>{item.cotizaciones.map((cotizacion) => cotizacion.descripcion) + "  "}</center></td>
-                            <td><center>{item.cotizaciones.map((cotizacion) => cotizacion.estado) + "  "}</center></td>
-                            <td><center><a href={`/cotizaciones/detalles/${item.cotizaciones.map((cotizacion) => cotizacion.id) + "  "}`}>{item.cotizaciones.map((cotizacion) => cotizacion.precio) + "  "}</a></center></td>
+                            <td><center>{item.cotizaciones.map((cotizacion) => cotizacion.agencia) + '  '}</center></td>
+                            <td><center>{item.cotizaciones.map((cotizacion) => cotizacion.descripcion) + '  '}</center></td>
+                            <td><center>{item.cotizaciones.map((cotizacion) => cotizacion.estado) + '  '}</center></td>
+                            <td><center><a href={`/cotizaciones/detalles/${item.cotizaciones.map((cotizacion) => cotizacion.id) + '  '}`}>{item.cotizaciones.map((cotizacion) => cotizacion.precio) + '  '}</a></center></td>
                           </tr>
-                        );
-                      })
-                    )}
+                              )
+                            })
+                          )}
                   </tbody>
                 </Table>
                 </div>
