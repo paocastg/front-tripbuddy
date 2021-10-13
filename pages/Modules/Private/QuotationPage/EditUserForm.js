2fd59ae1026e44db77be810e9151faa8400cd887
SumildaBengolea <sbengolea@2cloud.pe>
2021-10-13 14:21:49 -0500

    cambios en tabla solicitud

---
+++ pages/Modules/Private/QuotationPage/EditUserForm.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from "react";
+import { Table } from 'reactstrap'
+import moment from 'moment'
+
+const EditUserForm = props => {
+  const initialFormState = { agencia: null, descripcion: "", estado: "", lista: "" };
+  const [user, setUser] = useState(
+    props.editing ? props.currentUser : initialFormState
+  );
+  useEffect(() => {
+    setUser(props.currentUser);
+    console.log(user)
+    console.log('user')
+  }, [props]);
+
+  return (
+    <div>
+    <div style = {{ overflow: 'scroll' }}>
+    <Table style = {{ width: '1400px' }}>
+    <thead>
+    <tr style={{ background: '#00B2E3', color: '#fff', fontFamily: 'Geomanist-Regular', height: '60px' }}>
+                      <th>Agencia</th>
+                      <th>Descripcion</th>
+                      <th></th>
+                      <th>Destinos</th>
+                      <th></th>
+                      <th>Precio</th>
+                      <th></th>
+      </tr>
+      <tr style={{ background: '#2C2C2C', color: '#fff', fontFamily: 'Geomanist-Regular', width: '1400px' }}>
+                      <th></th>
+                      <th></th>
+                      <th></th>
+                      <th>Categoria</th>
+                      <th>Actividades</th>
+                      <th></th>
+                      </tr>
+    </thead>
+    <tbody>
+        {
+            <tr key={1} style={{
+                background: '#2C2C2C',
+                fontFamily: 'Geomanist-Regular',
+                color: '#fff',
+                cursor: 'pointer',
+                border: '0.5px solid'
+              }}>
+            <td>{user.destino && user.destino.map((destinos) => destinos.nombre) + '  '}</td>
+            <td>{'   / ' + ((moment(user.fecha_fin).diff(moment(user.fecha_inicio), 'dias')) / (1000 * 60 * 60 * 24)) + 'dias'}</td>
+            <td>{ ' / ' + (user.numero_adultos + user.numero_adolescentes + user.numero_ninos) + ' Personas ' }</td>
+            <td>{ user.categoria && user.categoria.map((categorias) => categorias.nombre) + '  ' }</td>
+            <td>{ user.actividad && user.actividad.map((actividads) => actividads.nombre) + ' ' }</td>
+            <td style = {{display: 'none'}}>{(user.cotizaciones) && (user.cotizaciones.map((item) => item.solicitud) + ' ')[0] }</td>
+            <td></td>          
+          </tr>
+        }
+      {
+      user.cotizaciones && user.cotizaciones.map((item) => 
+      <tr style={{ border: 'silver 2px solid' }}>
+      <td>{item.agencia}</td>
+      <td>{item.descripcion}</td>
+      <td></td>
+      <td>{item.estado}</td>
+      <td></td>
+      <td><center><a href={`/cotizaciones/detalles/${ item.id + '  '}`}>{ item.precio + '  '}</a></center></td>
+      </tr>
+      )
+      }
+    </tbody>
+  </Table>
+  </div>
+    </div>
+  );
+};
+
+export default EditUserForm;
--- pages/Modules/Private/QuotationPage/index.jsx
+++ pages/Modules/Private/QuotationPage/index.jsx
@@ -1,8 +1,9 @@
 import styles from './index.module.scss'
+import UserTable from "./UserTable";
+import EditUserForm from "./EditUserForm";
 /* Components */
 import { Table } from 'reactstrap'
 import H2 from 'components/H2'
-import moment from 'moment'
 
 /* Layout */
 import Session from 'layout/Session'
@@ -14,40 +15,31 @@ import { Auth } from 'assets/Utils/Auth'
 import Button from 'components/Button'
 
 const QuotationPage = () => {
-  const [data, setData] = useState([])
-  const [coti, setCoti] = useState('')
+  const initialFormState = { agencia: null, descripcion: "", estado: "", lista: "" };
+  const [users, setUsers] = useState('');
+  const [editing, setEditing] = useState(false);
+  const [currentUser, setCurrentUser] = useState(initialFormState);
   const fetchUsers = async () => {
     const user = Auth.getSession().usuario.id
-    const response = await axios.get(HOST + '/solicitud/list_cotizaciones/'+ user)
-    setData(response.data.solicitud)
-  }  
-  const Pruebados = () =>(                   
-    <tbody>
-      {
-    data && data.map((index) => 
-    index.cotizaciones.map((lista) => 
-    { if( lista.solicitud === 7 && lista.solicitud > 0 ){
-      console.log(lista.precio)
-        return (
-          <tr key={index} style={{ fontFamily: 'Geomanist-Regular', cursor: 'pointer' }}>
-          <td><center>{ lista.agencia + '  ' }</center></td>
-          <td><center>{ lista.descripcion + '  ' }</center></td>
-          <td><center>{ lista.estado + '  ' }</center></td>
-          <td><center><a href={`/cotizaciones/detalles/${ lista.id + '  '}`}>{ lista.precio + '  '}</a></center></td>
-          
-          </tr>
-         )
-        } else{
-          <h1> no existe cotizaciones</h1>
-        }
-    })
-  )
-    }
-  </tbody>)
+    const response = await axios.get(HOST + '/solicitud/list_cotizaciones/' + user)
+    setUsers(response.data.solicitud)
+  }
+  console.log(users)
   useEffect(() => {
     fetchUsers()
   }, [])
-  const esconder = () => {
+
+  const editRow = user => {
+    setEditing(true);
+    setCurrentUser(user);
+    esconder();
+  };
+  const updateUser = (solicitud, updatedUser) => {
+    console.log(solicitud)
+    setEditing(false);
+    setUsers(users.map(user => user.cotizaciones.map((item) => item.solicitud === solicitud ? updatedUser : user)));
+  };
+  const esconder = () => {        
     document.getElementById('solicitud').style.display = 'none'
     document.getElementById('cotizaciones').style.display = 'block'
   }
@@ -55,78 +47,29 @@ const QuotationPage = () => {
     document.getElementById('solicitud').style.display = 'block'
     document.getElementById('cotizaciones').style.display = 'none'
   }
-
   return (
     <Wrapper>
       <Session>
         <div className={styles.quotation}>
           <H2>Cotizaciones Abiertas</H2>
-          <div id='solicitud'>
-          <div style = {{ overflow: 'scroll' }}>
-          <Table style = {{ width: '1400px' }}>
-                  <thead>
-                    <tr style={{ background: '#00B2E3', color: '#fff', fontFamily: 'Geomanist-Regular', height: '60px' }}>
-                      <th>Agencia</th>
-                      <th>Descripcion</th>
-                      <th>Destinos</th>
-                      <th>Precio</th>
-                      <th></th>
-                    </tr><br></br>
-                    <tr style={{ background: '#2C2C2C', color: '#fff', fontFamily: 'Geomanist-Regular', width: '1400px' }}>
-                      <th></th>
-                      <th></th>
-                      <th>Categoria</th>
-                      <th>Actividades</th>
-                      <th>Solicitud</th>
-                      </tr>
-                  </thead>
-                  <tbody>
-                    {
-                      data && data.map((item, index, key) => {
-                        return (
-                          <tr key={key} style={{
-                            background: '#2C2C2C',
-                            fontFamily: 'Geomanist-Regular',
-                            color: '#fff',
-                            cursor: 'pointer'
-                          }}>
-                           <center><td>{ item.destino.map((destinos) => destinos.nombre) + '  ' }</td></center>
-                          <th>
-                          <td>{ '   / ' + ((moment(item.fecha_fin).diff(moment(item.fecha_inicio), 'dias')) / (1000 * 60 * 60 * 24)) + 'dias' }</td>
-                          <td>{ ' / ' + (item.numero_adultos + item.numero_adolescentes + item.numero_ninos) + ' Personas ' }</td>
-                          </th>
-                          <td>{ item.categoria.map((categorias) => categorias.nombre) + '  ' }</td>
-                          <td>{ item.actividad.map((actividads) => actividads.nombre) + ' ' }</td>
-                          <td>{<button style={{ background: '#2C2C2C' }} onClick={esconder}>{ item.cotizaciones.length + ' ' }</button>}</td>
-                          <br></br></tr>
-
-                        )
-                      })
-                    }
-                  </tbody>
-                </Table>
-              </div>
-          </div>
-          <div id='cotizaciones' style = {{display: 'none'}}>              
-          <div style = {{ overflow: 'scroll' }}>
-          <Table style = {{ width: '1400px' }}>
-                  <thead>
-                    <tr style={{ background: '#00B2E3', color: '#fff', fontFamily: 'Geomanist-Regular', height: '60px' }}>
-                      <th>Agencia</th>
-                      <th>Descripcion</th>
-                      <th>Destinos</th>
-                      <th>Precio</th>
-                      <th></th>
-                    </tr><br></br>
-                  </thead>
-                    <Pruebados></Pruebados>
-                </Table>
-                </div><br></br>
-                <center><div>
-                  <Button onClick={atras}>Atrás</Button>
-                </div></center>
+          <div id='cotizaciones' style = {{display: 'none'}}>
+            <EditUserForm
+              editing={editing}
+              setEditing={setEditing}
+              currentUser={currentUser}
+              setCurrentUser={setCurrentUser}
+              updateUser={updateUser}
+            /><br></br><br></br>
+            <center><div>
+              <Button onClick={atras}>Atrás</Button>
+            </div></center>
             </div>
-          </div>
+      <div id='solicitud'>
+        <div className="flex-large">
+          <UserTable users={users} editRow={editRow}/>
+        </div>
+      </div>          
+      </div>
       </Session>
     </Wrapper>
   )
