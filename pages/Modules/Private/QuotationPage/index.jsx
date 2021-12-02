667186d17d3c858bc764f94a70145b65a6be71dc
SumildaBengolea <sbengolea@2cloud.pe>
2021-12-02 15:54:19 -0500

    correccion

--- pages/Modules/Private/QuotationPage/index.jsx
+++ pages/Modules/Private/QuotationPage/index.jsx
@@ -10,35 +10,27 @@ import axios from 'axios'
 import React, { useState, useEffect } from 'react'
 import { Auth } from 'assets/Utils/Auth'
 import Button from 'components/Button'
-import { NextSeo } from 'next-seo'
 
 const QuotationPage = () => {
-  const initialFormState = {
-    agencia: null,
-    descripcion: '',
-    estado: '',
-    lista: ''
-  }
+  const initialFormState = { agencia: null, descripcion: '', estado: '', lista: '' }
   const [users, setUsers] = useState('')
   const [editing, setEditing] = useState(false)
   const [currentUser, setCurrentUser] = useState(initialFormState)
   // console.log(saved)
   const fetchUsers = async () => {
     const user = Auth.getSession().usuario.id
-
     const config = {
       // headers: { Authorization: `Token ${token}` },d4e97b7df5a2785717f9889d9c870525d3222f1a
        headers: { Authorization: `Token d4e97b7df5a2785717f9889d9c870525d3222f1a` },
     }
     const response = await axios.get(HOST + '/solicitud/list_cotizaciones/' + user, config)
-
     setUsers(response.data.solicitud)
   }
   useEffect(() => {
     fetchUsers()
   }, [])
 
-  const editRow = (user) => {
+  const editRow = user => {
     setEditing(true)
     setCurrentUser(user)
     esconder()
@@ -46,13 +38,7 @@ const QuotationPage = () => {
   const updateUser = (solicitud, updatedUser) => {
     console.log(solicitud)
     setEditing(false)
-    setUsers(
-      users.map((user) =>
-        user.cotizaciones.map((item) =>
-          item.solicitud === solicitud ? updatedUser : user
-        )
-      )
-    )
+    setUsers(users.map(user => user.cotizaciones.map((item) => item.solicitud === solicitud ? updatedUser : user)))
   }
   const esconder = () => {
     document.getElementById('solicitud').style.display = 'none'
@@ -65,27 +51,28 @@ const QuotationPage = () => {
   return (
     <Wrapper>
       <Session>
-        <NextSeo title="Cotizaciones" description="A short description goes here." />
         <div className={styles.quotation}>
+          <div style = {{ display: 'none' }}>
+            </div>
+          <div id='cotizaciones' style = {{ display: 'none' }}>
             <EditUserForm
               editing={editing}
               setEditing={setEditing}
               currentUser={currentUser}
               setCurrentUser={setCurrentUser}
               updateUser={updateUser}
-
-            />
-          <div id="solicitud">
-            <div className="flex-large">
-              <UserTable users={users} editRow={editRow} />
             /><br></br><br></br>
             <center><div>
               <Button onClick={atras}>Atrás</Button>
               <Button onClick={atras}>Eliminar viaje</Button>
             </div></center>
             </div>
-          </div>
+      <div id='solicitud'>
+        <div className="flex-large">
+          <UserTable users={users} editRow={editRow}/>
         </div>
+      </div>
+      </div>
       </Session>
     </Wrapper>
   )
